f1df3edfcb238cf5bb1f6ea17c09c8f1b12be251
karanpalsinghshekhawat <karanpal609@gmail.com>
2023-03-31 12:25:59 -0400

    huber location estimator added

---
+++ model/utils/data_transformation.py
@@ -0,0 +1,30 @@
+"""
+This module contains methods for data transformations
+mostly huber location estimator which is a general
+version of combination of median or mean if the underlying
+distribution has outliers.
+"""
+
+import numpy as np
+
+
+def huber_location_estimator(x, c=1.345):
+    """
+    Computes huber location estimator
+    for an
+
+    Args:
+        x: numpy array
+        c: tuning constant
+
+    Returns:
+        float
+    """
+    median = np.median(x)
+    mad = np.median(np.abs(x - median))
+    k = c * mad
+    w = np.zeros_like(x)
+    w[np.abs(x - median) <= k] = 1
+    estimator = np.sum(w * x) / np.sum(w)
+
+    return estimator
